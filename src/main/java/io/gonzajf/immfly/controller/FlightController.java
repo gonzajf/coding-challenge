7acb6c55dc9aff98fe61bd44d7683e79288dbb66
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-26 00:53:02 -0300

    passing get flight not found - controller test

--- src/main/java/io/gonzajf/immfly/controller/FlightController.java
+++ src/main/java/io/gonzajf/immfly/controller/FlightController.java
@@ -1,10 +1,14 @@
 package io.gonzajf.immfly.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.gonzajf.immfly.domain.Flight;
+import io.gonzajf.immfly.exception.FlightNotFoundException;
 import io.gonzajf.immfly.service.FlightService;
 
 @RestController
@@ -20,4 +24,9 @@ public class FlightController {
 	public Flight getFlight(@PathVariable String tailNumber, @PathVariable String flightNumber) {
 		return flightService.getFlightDetails(tailNumber, flightNumber);
 	}
+	
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	private void flightNotFoundHandler(FlightNotFoundException ex) {}
+	
 }
