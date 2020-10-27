792ee3ff006cdf1461e381399d6561c9564a940d
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-27 20:26:16 -0300

    passing test - get flight details not found in service

--- src/main/java/io/gonzajf/immfly/service/FlightService.java
+++ src/main/java/io/gonzajf/immfly/service/FlightService.java
@@ -2,6 +2,7 @@ package io.gonzajf.immfly.service;
 
 import io.gonzajf.immfly.domain.Flight;
 import io.gonzajf.immfly.dto.FlightDTO;
+import io.gonzajf.immfly.exception.FlightNotFoundException;
 import io.gonzajf.immfly.util.FlightClient;
 import io.gonzajf.immfly.util.FlightMapper;
 
@@ -15,6 +16,9 @@ public class FlightService {
 
 	public Flight getFlightDetails(String tailNumber, String flightNumber) {
 		FlightDTO fligthDTO = flightClient.getFlightDetails(tailNumber, flightNumber);
+		if(fligthDTO == null) {
+			throw new FlightNotFoundException();
+		}
 		return FlightMapper.dtoToObject(fligthDTO);
 	}
 
