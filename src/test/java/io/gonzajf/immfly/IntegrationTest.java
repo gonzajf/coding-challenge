5d5240e32117517038d12798f31f5759f547d1b3
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-29 20:15:16 -0300

    integration test passing

--- src/test/java/io/gonzajf/immfly/IntegrationTest.java
+++ src/test/java/io/gonzajf/immfly/IntegrationTest.java
@@ -1,6 +1,9 @@
 package io.gonzajf.immfly;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +12,44 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import io.gonzajf.immfly.domain.Flight;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public class IntegrationTest {
+
+	@Container
+	public static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:5.0.3-alpine"))
+		.withExposedPorts(6379);
 
 	@Autowired
 	private TestRestTemplate restTemplate;
+	
+
+	@DynamicPropertySource
+	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", redis::getHost);
+		registry.add("spring.redis.port", redis::getFirstMappedPort);
+	}
+
+	@BeforeClass
+	public void startContainer() {
+		redis.start();
+	}
+
+	@AfterClass
+	public void stopContainer() {
+		redis.stop();
+	}
 	
 	@Test
 	public void getFlight_shouldReturnFlightDetails() throws Exception {
